5bf6438f65ec8f8cb276c45b9fbc42dfa3d2922f
Nicolò Genesio (二コゲネ) <nicolo.genesio@iit.it>
2022-05-24 14:38:06 +0200

    Update packages_vars.sh

--- .ci/packages_vars.sh
+++ .ci/packages_vars.sh
@@ -11,21 +11,21 @@
 YCM_PACKAGE="ycm-cmake-modules"
 YCM_REQUIRED_VERSION="0.13.0"
 YCM_PACKAGE_URL_buster="https://github.com/robotology/ycm/releases/download/v${YCM_REQUIRED_VERSION}/${YCM_PACKAGE}_${YCM_REQUIRED_VERSION}-1.debian10.robotology1_all.deb"
-YCM_PACKAGE_URL_bionic="https://github.com/robotology/ycm/releases/download/v${YCM_REQUIRED_VERSION}/${YCM_PACKAGE}_${YCM_REQUIRED_VERSION}-1.ubuntu18.04.robotology1_all.deb"
 YCM_PACKAGE_URL_focal="https://github.com/robotology/ycm/releases/download/v${YCM_REQUIRED_VERSION}/${YCM_PACKAGE}_${YCM_REQUIRED_VERSION}-1.ubuntu20.04.robotology1_all.deb"
+YCM_PACKAGE_URL_jammy=$YCM_PACKAGE_URL_focal
 
 YARP_REQUIRED_VERSION="3.6.0"
 YARP_PACKAGE_URL_buster="https://github.com/robotology/yarp/releases/download/v${YARP_REQUIRED_VERSION}/yarp-${YARP_REQUIRED_VERSION}-2.buster_amd64.deb"
-YARP_PACKAGE_URL_bionic="https://github.com/robotology/yarp/releases/download/v${YARP_REQUIRED_VERSION}/yarp-${YARP_REQUIRED_VERSION}-2.bionic_amd64.deb"
 YARP_PACKAGE_URL_focal="https://github.com/robotology/yarp/releases/download/v${YARP_REQUIRED_VERSION}/yarp-${YARP_REQUIRED_VERSION}-2.focal_amd64.deb"
+YARP_PACKAGE_URL_jammy=$YARP_PACKAGE_URL_focal
 
 APT_OPTIONS="-q -y"
 
-SUPPORTED_DISTRO_LIST="buster bionic focal"
+SUPPORTED_DISTRO_LIST="buster jammy focal"
 SUPPORTED_TARGET_LIST="amd64"
 CMAKE_MIN_REQ_VER="3.16.0"
 ICUB_DEPS_COMMON="libace-dev libboost-filesystem-dev libboost-system-dev libboost-thread-dev libc6 python3 libgsl0-dev libncurses5-dev libsdl1.2-dev subversion git gfortran libxmu-dev libode-dev wget unzip qtbase5-dev qtdeclarative5-dev qtmultimedia5-dev libqt5svg5 libqt5opengl5-dev libopencv-dev freeglut3-dev libtinyxml-dev libblas-dev coinor-libipopt-dev liblapack-dev libmumps-dev qml-module-qtmultimedia qml-module-qtquick-dialogs qml-module-qtquick-controls qml-module-qt-labs-settings libqcustomplot-dev qml-module-qt-labs-folderlistmodel libedit-dev libeigen3-dev libjpeg-dev libsimbody-dev libxml2-dev libjs-underscore portaudio19-dev libgtkmm-2.4-dev ${YCM_PACKAGE}"
-ICUB_DEPS_bionic="libode6"
+ICUB_DEPS_jammy="libode8"
 ICUB_DEPS_focal="libode8"
 ICUB_DEPS_buster="libode8"
 ICUB_PACKAGE_MAINTAINER="Matteo Brunettini <matteo.brunettini@iit.it>"
@@ -49,5 +49,5 @@ ICUB_CMAKE_OPTIONS="\
  -DENABLE_icubmod_xsensmtx=OFF \
  -DYARP_FORCE_DYNAMIC_PLUGINS=ON"
 CMAKE_OPTIONS_focal=""
-CMAKE_OPTIONS_bionic=""
+CMAKE_OPTIONS_jammy=""
 CMAKE_OPTIONS_buster=""
