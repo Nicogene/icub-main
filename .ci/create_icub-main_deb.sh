57232db18a4c59cfdfdd6f623eec2820c0e70158
Nicolò Genesio (二コゲネ) <nicolo.genesio@iit.it>
2022-05-24 14:39:37 +0200

    Update create_icub-main_deb.sh

--- .ci/create_icub-main_deb.sh
+++ .ci/create_icub-main_deb.sh
@@ -283,11 +283,7 @@ install_deps()
   ###------------------- Handle cmake ----------------------###
   echo "Installing CMAKE in the environment"
 
-  if [ "$_PLATFORM_RELEASE" == "bionic" ]; then
-    wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | $_SUDO apt-key add -
-    $_SUDO apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'
-    DEBIAN_FRONTEND=noninteractive; $_SUDO apt-get install $APT_OPTIONS cmake
-  elif [ "$_PLATFORM_RELEASE" == "buster" ]; then
+  if [ "$_PLATFORM_RELEASE" == "buster" ]; then
     $_SUDO apt-add-repository 'deb http://deb.debian.org/debian buster-backports main'
     DEBIAN_FRONTEND=noninteractive;
     $_SUDO apt-get -y update && $_SUDO apt-get -y install -t buster-backports cmake
